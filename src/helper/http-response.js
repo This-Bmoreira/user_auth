8030429a4da625b79bd8517da8eea1e1e98fd8a0
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-09-12 14:30:12 -0300

    feat: add httpResponse utility class
    Added a utility class named 'httpResponse' with static methods for generating HTTP response objects.
    Each method returns an object containing a 'statusCode' field corresponding to the specified HTTP
    status code. Additionally, added methods for handling specific error cases like 'badRequest' and
    'unauthorized', which return objects with appropriate status codes and error bodies.

---
+++ src/helper/http-response.js
@@ -0,0 +1,294 @@
+import MissingParamError from './missing-param-error';
+import Unauthorized from './unauthorized-error';
+
+export default class httpResponse {
+  static continue() {
+    return {
+      statusCode: 100,
+    };
+  }
+
+  static switchingProtocols() {
+    return {
+      statusCode: 101,
+    };
+  }
+
+  static processing() {
+    return {
+      statusCode: 102,
+    };
+  }
+
+  static earlyHints() {
+    return {
+      statusCode: 103,
+    };
+  }
+
+  static ok() {
+    return {
+      statusCode: 200,
+    };
+  }
+
+  static created() {
+    return {
+      statusCode: 201,
+    };
+  }
+
+  static accepted() {
+    return {
+      statusCode: 202,
+    };
+  }
+
+  static nonAuthoritativeInformation() {
+    return {
+      statusCode: 203,
+    };
+  }
+
+  static noContent() {
+    return {
+      statusCode: 204,
+    };
+  }
+
+  static resetContent() {
+    return {
+      statusCode: 205,
+    };
+  }
+
+  static partialContent() {
+    return {
+      statusCode: 206,
+    };
+  }
+
+  static ambiguous() {
+    return {
+      statusCode: 300,
+    };
+  }
+
+  static movedPermanently() {
+    return {
+      statusCode: 301,
+    };
+  }
+
+  static found() {
+    return {
+      statusCode: 302,
+    };
+  }
+
+  static seeOther() {
+    return {
+      statusCode: 303,
+    };
+  }
+
+  static notModified() {
+    return {
+      statusCode: 304,
+    };
+  }
+
+  static temporaryRedirect() {
+    return {
+      statusCode: 307,
+    };
+  }
+
+  static permanentRedirect() {
+    return {
+      statusCode: 308,
+    };
+  }
+
+  static badRequest(paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName),
+    };
+  }
+
+  static unauthorized() {
+    return {
+      statusCode: 401,
+      body: new Unauthorized(),
+    };
+  }
+
+  static paymentRequired() {
+    return {
+      statusCode: 402,
+    };
+  }
+
+  static forbidden() {
+    return {
+      statusCode: 403,
+    };
+  }
+
+  static notFound() {
+    return {
+      statusCode: 404,
+    };
+  }
+
+  static methodNotAllowed() {
+    return {
+      statusCode: 405,
+    };
+  }
+
+  static notAcceptable() {
+    return {
+      statusCode: 406,
+    };
+  }
+
+  static proxyAuthenticationRequired() {
+    return {
+      statusCode: 407,
+    };
+  }
+
+  static requestTimeout() {
+    return {
+      statusCode: 408,
+    };
+  }
+
+  static conflict() {
+    return {
+      statusCode: 409,
+    };
+  }
+
+  static gone() {
+    return {
+      statusCode: 410,
+    };
+  }
+
+  static lengthRequired() {
+    return {
+      statusCode: 411,
+    };
+  }
+
+  static preconditionFailed() {
+    return {
+      statusCode: 412,
+    };
+  }
+
+  static payloadTooLarge() {
+    return {
+      statusCode: 413,
+    };
+  }
+
+  static uriTooLong() {
+    return {
+      statusCode: 414,
+    };
+  }
+
+  static unsupportedMediaType() {
+    return {
+      statusCode: 415,
+    };
+  }
+
+  static requestedRangeNotSatisfiable() {
+    return {
+      statusCode: 416,
+    };
+  }
+
+  static expectationFailed() {
+    return {
+      statusCode: 417,
+    };
+  }
+
+  static iAmATeapot() {
+    return {
+      statusCode: 418,
+    };
+  }
+
+  static misdirected() {
+    return {
+      statusCode: 421,
+    };
+  }
+
+  static unprocessableEntity() {
+    return {
+      statusCode: 422,
+    };
+  }
+
+  static failedDependency() {
+    return {
+      statusCode: 424,
+    };
+  }
+
+  static preconditionRequired() {
+    return {
+      statusCode: 428,
+    };
+  }
+
+  static tooManyRequests() {
+    return {
+      statusCode: 429,
+    };
+  }
+
+  static internalServerError() {
+    return {
+      statusCode: 500,
+    };
+  }
+
+  static notImplemented() {
+    return {
+      statusCode: 501,
+    };
+  }
+
+  static badGateway() {
+    return {
+      statusCode: 502,
+    };
+  }
+
+  static serviceUnavailable() {
+    return {
+      statusCode: 503,
+    };
+  }
+
+  static gatewayTimeout() {
+    return {
+      statusCode: 504,
+    };
+  }
+
+  static httpVersionNotSupported() {
+    return {
+      statusCode: 505,
+    };
+  }
+}
