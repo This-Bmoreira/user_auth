b1066fd196341accb344face6e9995cf1d140e56
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-09-19 10:04:19 -0300

    feat: improve email validation and error handling

--- src/util/email-validator.js
+++ src/util/email-validator.js
@@ -1,7 +1,11 @@
 import validator from 'validator';
+import MissingParamError from './error/missing-param-error';
 
 export default class EmailValidator {
   isValid(email) {
+    if (!email) {
+      throw new MissingParamError('email');
+    }
     return validator.isEmail(email);
   }
 }
